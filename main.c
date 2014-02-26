c0856f1633ec67c716ed2a7c4acf91e01756ac0b
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2014-02-26 12:23:12 -0500

    First iteration of shell, correctly captures and handles "exit" keyword and ctrl-d. Can also detect "&" character at end of line, for eventual forking purposes.

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
 #define MAX_SIZE 1024
 
 int main(void)
 {
+    int IS_FORKING = 0;
     while(1) {
         printf("shell> ");
         char* input = malloc(sizeof(char) * MAX_SIZE);
-        char* output = fgets(input, MAX_SIZE, stdin);
-        printf(output);
+        const char* output = fgets(input, MAX_SIZE, stdin);
+        int len = strlen(output);
+        const char* lastTwoChars = &output[len-2];
+        if (feof(stdin) || (strcmp(output, "exit\n") == 0)) {
+            printf("Exiting!\n");
+            exit(0);
+        }
+        if (strcmp(lastTwoChars, "&\n") == 0) {
+            IS_FORKING = 1;
+        }
+        printf("%d\n", IS_FORKING); // replace with parse_input call
     }
 }
